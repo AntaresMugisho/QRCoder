bc3b082871730fba34c7fafb04c4f44d74c75b3f
AntaresMugisho <antaresmugisho@gmail.com>
2023-09-28 19:50:52 +0200

    Update app description

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 
 
 
-const scanner = new Html5QrcodeScanner()
+const scanner = new Html5QrcodeScanner("reader", {
+    qrbox:{
+        width:250,
+        height:250,
+    },
+    fps:20,
+})
+
+scanner.render(onSuccess, onError)
+
+function onSuccess(result){
+    document.getElementById("result").innerHTML = `
+    <h2>Success !</h2>
+    <a href="${result}">${result}</a>
+    `
+
+    scanner.clear()
+
+    document.getElementById("reader").remove()
+}
+
+function onError(error){
+    console.log(error)
+}
